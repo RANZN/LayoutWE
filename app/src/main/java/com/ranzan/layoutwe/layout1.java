5cacd982b8df3bda6b43e8a68db999088618cccc
PRAKASH RANJAN <aryaprakashranjan@gmail.com>
2021-06-30 01:28:43 +0530

    WE PART DONE
    Layout 1 -- q1;
    Layout 2 -- q2;

---
+++ app/src/main/java/com/ranzan/layoutwe/layout1.java
@@ -0,0 +1,15 @@
+package com.ranzan.layoutwe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class layout1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout1);
+    }
+}
